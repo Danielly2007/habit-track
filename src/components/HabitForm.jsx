da512eaf46c44157b22414eebd842fee2d990f85
Danielly <daniellynerylove1@gmail.com>
2025-09-23 14:50:27 -0300

    feat: Create HabitForm

---
+++ src/components/HabitForm.jsx
@@ -0,0 +1,28 @@
+import { useState, useContext } from "react";
+import { HabitContext } from "../context/HabitContext";
+
+const HabitForm = () => {
+  const [title, setTitle] = useState("");
+  const { addHabit } = useContext(HabitContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addHabit(title);
+      setTitle("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Novo hábito"
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+};
+
+export default HabitForm;
